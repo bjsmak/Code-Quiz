6497f9b3858bf0c2d30a165810bcf1e193f70cf9
Brian Smak <brian.j.smak@gmail.com>
2020-10-03 21:02:56 -0400

    questions check function added, high score logs

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var answerButtonB = document.querySelector('#answerB');
 var answerButtonC = document.querySelector('#answerC');
 var answerButtonD = document.querySelector('#answerD');
 var timeEl = document.querySelector('#clock');
+var scorePage = document.querySelector('#score');
 
 //Quiz Questions referenced in an objects/keys matrix
 var questionsArr = [{
@@ -17,7 +18,7 @@ var questionsArr = [{
     answerB: "meta",
     answerC: "script",
     answerD: "header",
-    correctAnswer: "C"
+    correctAnswer: "nav"
     },
     {
     question: "What element allows the user to manipulate what a button does when it is clicked?",
@@ -25,15 +26,15 @@ var questionsArr = [{
     answerB: ".querySelector",
     answerC: ".button",
     answerD: ".addEventListener",
-    correctAnswer: "D"
+    correctAnswer: ".addEventListener"
     },
     {
-    question: "What HTML element references the Javascript files?",
-    answerA: "nav",
-    answerB: "meta",
-    answerC: "script",
-    answerD: "header",
-    correctAnswer: "D"
+    question: "What precedes an id?",
+    answerA: ".",
+    answerB: "#",
+    answerC: "?",
+    answerD: "@",
+    correctAnswer: "#"
     },
     {
     question: "What HTML element references the Javascript files?",
@@ -45,25 +46,24 @@ var questionsArr = [{
 
 //Button to start quiz and answer buttons
 startQuizButton.addEventListener("click", startQuiz);
-answerButtonA.addEventListener("click", checkQuestion('A'));
-answerButtonB.addEventListener("click", checkQuestion('B'));
-answerButtonC.addEventListener("click", checkQuestion('C'));
-answerButtonD.addEventListener("click", checkQuestion('D'));
+answerButtonA.addEventListener("click", checkQuestion);
+answerButtonB.addEventListener("click", checkQuestion);
+answerButtonC.addEventListener("click", checkQuestion);
+answerButtonD.addEventListener("click", checkQuestion);
 
-//Seconds on timer
+ 
+//Seconds on timer, timer interval as global
 var secondsLeft = 60;
-var score = 0;
-var questionIndex;
-
+var questionIndex = 0;
+var timerInterval;
 
 function startQuiz () {
     //Remove start page and begin questions
     startQuizDisplay.classList.add("hide");
     questionPageDisplay.classList.remove("hide");
-    addQuestion(0);
+    addQuestion();
 
-    
-       var timerInterval = setInterval(function() {
+     timerInterval = setInterval(function() {
         secondsLeft--;
         //NEED TO DEFINE timeEL in HTML - display clock
         timeEl.textContent = secondsLeft + " seconds left.";
@@ -77,29 +77,64 @@ function startQuiz () {
 
 }
 //Adds questions to display/answers
-function addQuestion(questionIndex) {
+function addQuestion() {
     console.log(questionIndex);
     questionEl.innerText = questionsArr[questionIndex].question;
     answerA.innerHTML = questionsArr[questionIndex].answerA;
     answerB.innerHTML = questionsArr[questionIndex].answerB;
     answerC.innerHTML = questionsArr[questionIndex].answerC;
     answerD.innerHTML = questionsArr[questionIndex].answerD;
+    document.querySelector('.message').innerText = '';
 }
 
 
-function checkQuestion(answer) {
-    correctAnswer = questionsArr["correctAnswer"];
+function checkQuestion() {
+    correctAnswer = questionsArr[questionIndex].correctAnswer;
+    var answer = this.innerText;
+        console.log(this.innerText , correctAnswer);
+        questionIndex++
     if (answer === correctAnswer){
-        score++;
-        addQuestion(1);
+        document.querySelector('.message').innerText = 'correct!'
     }
-    else {secondsLeft = secondsLeft-10;
-        addQuestion(1);}
+    else {
+        secondsLeft = secondsLeft-10;
+        document.querySelector('.message').innerText = 'incorrect!'
+    }
+
+    clearInterval(timerInterval);
+
+
+    //addQuestion();
+    if (questionIndex >= questionsArr.length) {
+        gameOver()
+    }else{setTimeout(startQuiz, 3000)};
+        
 }
 
 function gameOver() {
     questionPageDisplay.classList.add("hide");
     highScorePage.classList.remove('hide');
+    scorePage.innerHTML = secondsLeft;
+    document.querySelector('.message').innerText = '';
+}
+
+//On enter to submit initials and append name to local storage with high score
+document.querySelector("click", saveHighScore);
+
+//Function to save high score after game over
+function saveHighScore() {
+    var userName = document.querySelector('#name-text').value;
+    var scoreTotals = localStorage.getItem("highScoreList");
+
+    if (scoreTotals === null) {
+        localStorage.setItem("highScoreName", JSON.stringify(userName));
+        localStorage.setItem("highScore", JSON.stringify(secondsLeft));
+    } else {
+        scoreTotals = JSON.parse(scoreTotals);
+        localStorage.setItem("highScoreName", JSON.stringify(userName));
+        localStorage.setItem("highScoreName", JSON.stringify(secondsLeft));
+    }
+    document.querySelector('#high-scores').textContent = scoreTotals;
     
 }
 
